c87e616e4c84c85e11e2f34debab51fc90d1ede4
Nuruzzamanshah <mnueuzzaman@gmail.com>
2022-05-22 16:37:46 +0600

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,5 +3,20 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        oaktheme: {
+          primary: "#023430",
+          secondary: "#00684a",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
   plugins: [require("daisyui")],
 }
